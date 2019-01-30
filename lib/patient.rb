32708981b66c62bfa2276f568a8b8b3edca01539
Victoria B <viktorija.bourassa@gmail.com>
2019-01-30 11:32:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,7 +13,7 @@ class Patient
   end
 
    def new_appointment(doctor, date)
-    #creates new appt which should know that it belongs to the patient
+      Appointment.new()#creates new appt which should know that it belongs to the patient
   end
 
    def appointments
